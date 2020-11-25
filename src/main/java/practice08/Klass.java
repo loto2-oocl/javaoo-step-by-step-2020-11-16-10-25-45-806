62c105f7bb8208d3e8b5f0a2add886b74b1bff40
Tom Lo <loto2@oocl.com>
2020-11-25 16:44:17 +0800

    [java-oo][Tom] feat: implement Practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,33 @@
 package practice08;
 
 public class Klass {
+    private final Integer number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (leader.getKlass().getNumber().equals(this.getNumber())) {
+            this.leader = leader;
+        }
+        System.out.println("It is not one of us.");
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.getNumber());
+    }
+
+    public void appendMember(Student student) {
+
+    }
 }
