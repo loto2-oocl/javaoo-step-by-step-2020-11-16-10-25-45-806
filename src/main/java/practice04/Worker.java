e0f4fa0157d2e5350515567b6858f185a725cc0a
Tom Lo <loto2@oocl.com>
2020-11-25 14:44:52 +0800

    [java-oo][Tom] feat: implement Practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a %s. I have a job.", super.introduce(), this.getClass().getSimpleName());
+    }
 }
