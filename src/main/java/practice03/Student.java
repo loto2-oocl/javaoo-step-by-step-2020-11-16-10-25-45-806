0014d03faadf371545812ed07c4e33c300a49e73
Tom Lo <loto2@oocl.com>
2020-11-25 14:44:37 +0800

    [java-oo][Tom] feat: implement Practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,19 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a %s. I am at Class %d.", this.getClass().getSimpleName(), this.getKlass());
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,12 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a %s. I have a job.", this.getClass().getSimpleName());
+    }
 }
