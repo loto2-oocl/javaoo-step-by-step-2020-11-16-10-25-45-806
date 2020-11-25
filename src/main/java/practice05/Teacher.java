423e1e24937360416c0755e6bbca4497de11014e
Tom Lo <loto2@oocl.com>
2020-11-25 14:52:25 +0800

    [java-oo][Tom] feat: implement Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String getKlassString() {
+        if (this.getKlass() == null) {
+            return "No Class";
+        }
+        return String.format("Class %d", this.getKlass());
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a %s. I teach %s.", super.introduce(), this.getClass().getSimpleName(), this.getKlassString());
+    }
 }
